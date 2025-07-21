0b55b05b639cd50a38bfa8820fda78a52cf3111f
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-07-21 21:06:19 +0545

    added domin in cookie

--- dist/helpers/cookieOption.js
+++ dist/helpers/cookieOption.js
@@ -5,5 +5,6 @@ exports.cookieOptions = {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
+    domain: '.moneyhub.store',
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
--- src/helpers/cookieOption.ts
+++ src/helpers/cookieOption.ts
@@ -4,5 +4,6 @@ export const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: 'none',
+  domain: '.moneyhub.store',
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
