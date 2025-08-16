9499db49167153c66aedd873867f252c358606d3
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-08-16 12:36:02 +0545

    fixed bug

--- src/helpers/cookieOption.ts
+++ src/helpers/cookieOption.ts
@@ -4,6 +4,6 @@ export const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: 'none',
-  // domain: '.moneyhub.store',
+   domain: '.moneyhub.store',
   maxAge: 60 * 24 * 60 * 60 * 1000,
 };
